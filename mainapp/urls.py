1394025c895440d92766f08a32f1ae25de0139ff
t-kostin <timur.kostin@gmai.com>
2021-08-02 19:55:36 +0300

    lesson-5 unfinished

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import products
 
 app_name = 'mainapp'
 urlpatterns = [
-    path('', index, name='index'),
+    path('', products, name='index'),
+    path('category/<int:pk>/', products, name='category'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,25 +1,46 @@
-from django.shortcuts import render
-from json import loads
+from django.shortcuts import render, get_object_or_404
+# from json import loads
 from mainapp.models import Product, ProductCategory
 
 
-def index(request):
+def products(request, pk=None):
     title = 'продукты'
 
     # with open('mainapp/menu_links.json', 'r', encoding='utf-8') as source:
     #     menu_links = loads(source.read())
 
-    menu_links = [
-        {'href': 'index', 'name': 'все'},
-    ]
-    for element in ProductCategory.objects.all():
-        menu_links.append({'href': element.href, 'name': element.name})
+    # menu_links = [
+    #     {'href': 'index', 'name': 'все'},
+    # ]
+    # for element in ProductCategory.objects.all():
+    #     menu_links.append({'href': element.href, 'name': element.name})
 
-    products = Product.objects.all()[1:4]
+    menu_links = ProductCategory.objects.all()
+    related_products = Product.objects.all()[:3]
+
+    if pk is not None:
+        if pk == 0:
+            products = Product.objects.all().order_by('price')
+            category = {'name': 'все'}
+        else:
+            category = get_object_or_404(ProductCategory, pk=pk)
+            products = Product.objects.filter(category__pk=pk).order_by('price')
+        context = {
+            'title': title,
+            'menu_links': menu_links,
+            'related_products': related_products,
+            'category': category,
+            'products': products,
+        }
+        return render(request, 'mainapp/products.html', context)
+
+    related_products = Product.objects.all()[:3]
+    products = Product.objects.all().order_by('price')
+
     context = {
         'title': title,
         'menu_links': menu_links,
+        'related_products': related_products,
         'products': products,
-
     }
     return render(request, 'mainapp/products.html', context)
